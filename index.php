7663a9861ab0b26c1f1e914cdd41aa94386d2855
triplefire99 <triplefire1@gmail.com>
2017-02-04 23:40:03 -0500

    index.php

---
+++ index.php
@@ -0,0 +1,69 @@
+<html>
+<head>
+  <!-- materialize css -->
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/css/materialize.min.css">
+
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/js/materialize.min.js"></script>
+  <!--Import Google Icon Font-->
+    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+</head>
+  <div class="row">
+    <nav>
+        <div class="nav-wrapper deep-purple darken-2">
+          <div class="col s12">
+            <a href="#" class="brand-logo">Logo</a>
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
+              <li><a href=-"#about">About</a><11>
+              <li><a href="projects">Project</a></11>
+              <li><a href="contact">Contacts</a></11>
+            </ul>
+          </div>
+        </div>
+    </nav>
+  </div>
+<body>
+  <div class="container">
+    <div class="row">
+      <h1>Title</h1>
+      <p> Hello </p>
+
+      <div class="col s4">
+        <p>Test 1</p>
+      </div>
+      <div class="col s4">
+        <p>Test 2</p>
+      </div>
+      <div class="col s4">
+        <p>Test 3</p>
+      </div>
+    </div>
+  </div>
+  <footer class="page-footer purple">
+       <div class="container">
+         <div class="row">
+           <div class="col l6 s12">
+             <h5 class="white-text">Habib U</h5>
+             <p class="grey-text text-lighten-4">Hello, You suck!</p>
+           </div>
+           <div class="col l4 offset-l2 s12">
+             <h5 class="white-text">Links</h5>
+             <ul>
+               <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
+               <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
+               <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
+               <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+             </ul>
+           </div>
+         </div>
+       </div>
+       <div class="footer-copyright">
+         <div class="container">
+         © 2014 Copyright Text
+         <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+         </div>
+       </div>
+     </footer>
+</body>
+</html>
